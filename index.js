59dd344bc8ba1b8bbaa80122e88c9e6b4929333e
Shubham Kumar <78928613+shubham987654321@users.noreply.github.com>
2022-07-23 23:57:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+document.querySelectorAll('legend').forEach((item, index)=>{
+
+  let  form_collapse_index = document.getElementsByTagName('form')[index].children[0].children[1]
+  item.addEventListener("click", ()=>{
+    console.log(item)
+    form_collapse_index.classList.toggle("Collapse");
+  })
+})
